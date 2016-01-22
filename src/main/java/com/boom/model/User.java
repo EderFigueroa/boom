7138c0a5096fc68694641d08d9faf5b383c2fa8c
EderFigueroa <ederfigueroa.cs@gmail.com>
2016-01-22 10:35:04 -0800

    Starting porting to postgres

--- src/main/java/com/boom/model/User.java
+++ src/main/java/com/boom/model/User.java
@@ -29,4 +29,35 @@ public class User{
     return String.format("User[id=%d]", id);
   }
 
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean getEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
 }
